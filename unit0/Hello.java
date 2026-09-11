b7a755f0e7d6fd287515f4e7f68d14a2701d7117
sisi cherry <siennacherry7@gmail.com>
2026-09-11 14:27:46 -0700

    first hw

---
+++ unit0/Hello.java
@@ -0,0 +1,7 @@
+public class Hello 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Hello my neighbor goodbye see you later x hey its me, its verity!");
+    }
+}
